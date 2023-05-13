9207e2a0c699e822b77bdc5b5303a7ce98bf1322
Nedilio <izquierdonelson@gmail.com>
2023-05-13 02:12:08 -0400

    🆕 feat: react and darkmode config

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,27 @@
+﻿import { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(() => {
+    const theme = localStorage.getItem("theme");
+    console.log(theme);
+    return theme || "light";
+  });
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  return (
+    <button onClick={handleClick}>{theme === "light" ? "🌙" : "🌞"}</button>
+  );
+};
+
+export default ThemeToggle;
